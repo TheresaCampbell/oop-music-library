8bd16867e581640e93340be1872b68b450bcf7b1
Theresa Campbell <t.campbell@sasktel.net>
2018-07-13 18:18:21 +0000

    Added getter methods to playlist class that calculate total duration and overall rating of all tracks in a playlist.

--- library.js
+++ library.js
@@ -4,6 +4,10 @@ class Library {
     this.creator = creator;
     this.playlists = []
   }
+
+  addPlaylist(playlist) {
+    this.playlists.push(playlist);
+  }
 }
 
 
@@ -17,9 +21,25 @@ class Playlist {
     this.tracks.push(track);
   }
 
-  // get overallRating() {
+  get overallRating() {
+    let sumRating = 0;
+    let overallRating = null;
 
-  // }
+    for (const track of this.tracks) {
+      sumRating += track.rating;
+    }
+    overallRating = sumRating / this.tracks.length;
+    return overallRating;
+  }
+
+  get totalDuration() {
+    let totalDuration = 0;
+
+    for (const track of this.tracks) {
+      totalDuration += track.length;
+    }
+    return totalDuration;
+  }
 }
 
 
@@ -31,6 +51,8 @@ class Track {
   }
 }
 
+let library1 = new Library('Tunes', 'Theresa');
+
 let track1 = new Track('Como Te Quiero', 5, 180);
 let track2 = new Track('Sunday', 5, 120);
 let track3 = new Track('Papaoutai', 5, 150);
@@ -40,7 +62,8 @@ let playlist1 = new Playlist('Summer Jamz');
 playlist1.addTrack(track1);
 playlist1.addTrack(track2);
 playlist1.addTrack(track3);
-console.log(playlist1);
+
+library1.addPlaylist(playlist1);
 
 let track4 = new Track('River Takes the Town', 4, 180);
 let track5 = new Track('Trophy V6', 3, 120);
@@ -52,4 +75,6 @@ playlist2.addTrack(track4);
 playlist2.addTrack(track5);
 playlist2.addTrack(track6);
 
-console.log(playlist2);
+library1.addPlaylist(playlist2);
+
+console.log(playlist1.totalDuration);
